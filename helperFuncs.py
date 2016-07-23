3d21ce39cb152453e984c676e51ef732b7ab0e76
gogetter1707 <gogetter1707@gmail.com>
2016-07-23 15:56:28 -0700

    added check username/password

--- helperFuncs.py
+++ helperFuncs.py
@@ -1,5 +1,20 @@
-def checkUser(userName, password):
-	if len(userName) < 6:
-		print "Invalid UserName"
+# return -1 if username is less than 6 chars
+# return -2 if password is less than 6 chars 
+# return -3 if special chars in username 
+# return -4 if special chars in password 
+# return 1  if successful 
+
+
+def checkuser(username,password):
+	if len(username) < 6 : 
 		return -1
-	return 1
+
+	if len(password) < 6 :
+		return -2 
+	chars = set('!@#$%^&*?,<>'":;+= ') 
+	if any((c in chars) for c in username): 
+		return -3 
+	if any((c in chars) for c in password): 
+		return -4 
+	else:
+		return 1
